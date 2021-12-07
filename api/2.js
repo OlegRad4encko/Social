b671dad1cf5b565253a0d194ef1d5eacfb2b0bb8
OlegRad4encko <oleg.rad4encko@gmail.com>
2021-12-07 17:22:27 +0200

    add npm project

---
+++ api/2.js
@@ -0,0 +1,7 @@
+const test = inputString => `Hello ${inputString}`;
+const test1 = inputString => `Hello ${inputString}`
+
+module.exports = {
+    test,
+    test1
+};
---
+++ api/index.js
@@ -0,0 +1,19 @@
+const {test, test1 } = require('./2.js');
+
+console.log(test("007"));
+console.log(test1("008"));
+
+// const http = require('http');
+//
+// const hostname = '127.0.0.1';
+// const port = 3000;
+//
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World');
+// });
+//
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
