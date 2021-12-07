013ca21f81ff50bf7bf3d6392d5026fe185f5344
OlegRad4encko <oleg.rad4encko@gmail.com>
2021-12-07 18:32:31 +0200

    dev_api

--- api/index.js
+++ api/index.js
@@ -1,19 +1,24 @@
-const {test, test1 } = require('./2.js');
+// const {test, test1 } = require('./2.js');
+//
+// console.log(test("007"));
+// console.log(test1("008"));
 
-console.log(test("007"));
-console.log(test1("008"));
+const http = require('http');
 
-// const http = require('http');
-//
-// const hostname = '127.0.0.1';
-// const port = 3000;
-//
-// const server = http.createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.end('Hello World');
-// });
-//
-// server.listen(port, hostname, () => {
-//     console.log(`Server running at http://${hostname}:${port}/`);
-// });
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    console.log('start');
+    for (let i = 0; i < 1e9; i++) {
+
+    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+    console.log('end');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
